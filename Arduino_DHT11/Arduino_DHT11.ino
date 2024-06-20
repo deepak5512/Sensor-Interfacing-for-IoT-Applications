2bece55bda181afa09df76242279ba22c716aa34
Deepak Bhatter <120327568+deepak5512@users.noreply.github.com>
2024-06-20 11:45:13 +0530

    Arduino with DHT11

---
+++ Arduino_DHT11/Arduino_DHT11.ino
@@ -0,0 +1,33 @@
+#include "DHT.h"
+
+#define DHTPIN A0
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println(F("DHTxx test!"));
+
+  dht.begin();
+}
+
+void loop() {
+  // Wait a few seconds between measurements.
+  delay(2000);
+
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+
+  Serial.print("Humidity:");
+  Serial.print(h);
+  Serial.print(",");
+  Serial.print("Temperature:");
+  Serial.println(t);
+}
